ebabc59e6858b19543610db9815e6f2f52a1a519
Natsuki Tanaka <n2kia4@gmail.com>
2018-11-16 21:54:53 +0900

    Add import bookmarks from html file

--- src/cmd/import.rs
+++ src/cmd/import.rs
@@ -1,9 +1,32 @@
 use clap::{App, ArgMatches, SubCommand};
 
+use database::DB;
+use utils::get_bookmarks_from_html;
+
 pub fn make_subcommand<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("import")
         .about("Import bookmark")
+        .arg_from_usage("<FILE> 'Import bookmarks from html file'")
 }
 
 pub fn execute(args: &ArgMatches) {
+    let db = DB::open();
+    let file_name = args.value_of("FILE").unwrap();
+    let bookmarks = get_bookmarks_from_html(file_name);
+
+    for bookmark in bookmarks {
+        match db.add_bookmark(&bookmark.title, &bookmark.url) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("{} for \"{}\"\n", e, &bookmark.url);
+                continue;
+            }
+        }
+
+        for tag in &bookmark.tags {
+            db.add_tag(bookmark.id, tag);
+        }
+
+        bookmark.print();
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,45 @@
 use reqwest;
 use select::document::Document;
 use select::predicate::Name;
+use std::fs::File;
+use std::io::BufReader;
+
+use bookmark::Bookmark;
+
+pub fn get_bookmarks_from_html(file_name: &str) -> Vec<Bookmark> {
+    let file = File::open(file_name)
+        .expect("File not found or cannot be opened");
+    let doc = Document::from_read(BufReader::new(&file)).unwrap();
+    let mut bookmarks: Vec<Bookmark> = Vec::new();
+
+    for (i, a) in doc.find(Name("a")).enumerate() {
+        let mut tags: Vec<String> = Vec::new();
+
+        if let Some(f) = a.parent().unwrap().parent().unwrap()
+            .parent().unwrap().find(Name("h3")).next() {
+            tags.push(f.text());
+        }
+
+        if let Some(t) = a.attr("tags") {
+            for tag in t.split(",") {
+                tags.push(tag.to_string());
+            }
+        }
+
+        tags.sort();
+        tags.dedup();
+
+        let bookmark = Bookmark::new(
+            (i + 1) as i64,
+            a.text(),
+            a.attr("href").unwrap().to_string(),
+            tags,
+        );
+        bookmarks.push(bookmark);
+    }
+
+    bookmarks
+}
 
 pub fn get_title_from_url(url: &str) -> String {
     let res = reqwest::get(url).expect("invalid URL");
